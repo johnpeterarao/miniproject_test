d97effadcd256b17346acda8a12e76393a8f96ef
john <johnpeterarao@gmail.com>
2025-10-03 12:22:30 +0800

    changes for creating structure and some images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Inter } from "next/font/google";
 import "./globals.css";
+import '../../styles/global.scss';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'], 
+  variable: '--font-roboto',
+  display: 'swap',
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  subsets: ['latin'],
+  weight: '900', 
+  variable: '--font-inter',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${roboto.variable} ${inter.variable}`}>
         {children}
       </body>
     </html>
---
+++ src/components/header.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+
+
+export default function Header() {
+    return (
+       <header className="header">
+            <div className="thinbar">
+                <div className="container">
+                    <p>free shipping on us orders over $100</p>
+                </div>
+            </div>
+            <div className="header_main">
+                <div className="container">
+                    <div className="header_holder">
+                        <div className="header_left">
+                            <div className="header_burger">
+                                <Image
+                                    src="/assets/burgerMenu.svg" 
+                                    alt="Logo"
+                                    width={32}
+                                    height={22}
+                                />
+                            </div>
+                            <div className="header_logo">
+                                <Image
+                                    src="/assets/logo.svg" 
+                                    alt="Logo"
+                                    width={42}
+                                    height={55}
+                                />
+                            </div>
+                        </div>
+                        <div className="header_right">
+                            <button className="btn btn-dark">Get Started</button>
+                            <div className="header_cart">
+                                <Image
+                                    src="/assets/cart-icon.png" 
+                                    alt="Logo"
+                                    width={37}
+                                    height={33}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </header>
+    )
+}
